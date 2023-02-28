5cec633bc350afe736ad79085501962e8a5f08f7
XaN0037 <xannodirbek@gmail.com>
2023-02-28 18:05:18 +0500

    register and login

--- apps/v1/user/urls.py
+++ apps/v1/user/urls.py
@@ -3,5 +3,7 @@ from apps.v1.user import views
 
 urlpatterns = [
     path('register/', views.register, name='user_register'),
-    path('login/', views.login, name='user_login'),
+    path('register/reset/', views.reset, name='register_reset'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
 ]
